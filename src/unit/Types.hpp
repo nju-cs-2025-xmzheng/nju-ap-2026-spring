5c258df1185285eb13ba3323f18373a9f21b83b0
sprite <ximingzheng@smail.nju.edu.cn>
2026-05-26 00:41:29 +0800

    chore: move model type declarations from Unit.hpp to Types.hpp

--- src/unit/Types.hpp
+++ src/unit/Types.hpp
@@ -4,6 +4,10 @@
 
 namespace Synera::unit {
 
+enum class Owner { PlayerCtrl, EnemyCtrl };
+enum class State { Idle, Moving, Attacking, Casting, Dead };
+enum class Element { Anemo, Geo, Electro, Cryo, Pyro, Hydro };
+
 struct PyroDrop {};
 struct HydroDrop {};
 struct AnemoDrop {};
@@ -13,5 +17,29 @@ struct CryoDrop {};
 
 using Equipment = std::variant<std::monostate, PyroDrop, HydroDrop, AnemoDrop,
                                GeoDrop, ElectroDrop, CryoDrop>;
+
+struct UnitStats {
+    Owner owner = Owner::PlayerCtrl;
+    State state = State::Idle;
+    int hp = 0;
+    int max_hp = 0;
+    int atk = 0;
+    int range = 0;
+    int max_mana = 0;
+    int mana = 0;
+    int level = 1;
+    int shield = 0;
+    Equipment equipped = std::monostate{};
+};
+
+struct PyroSlime;
+struct HydroSlime;
+struct AnemoSlime;
+struct GeoSlime;
+struct ElectroSlime;
+struct CryoSlime;
+
+using Unit = std::variant<PyroSlime, HydroSlime, AnemoSlime, GeoSlime,
+                          ElectroSlime, CryoSlime>;
 
 } // namespace Synera::unit
--- src/unit/Unit.hpp
+++ src/unit/Unit.hpp
@@ -13,34 +13,6 @@ class Board;
 
 namespace Synera::unit {
 
-enum class Owner { PlayerCtrl, EnemyCtrl };
-enum class State { Idle, Moving, Attacking, Casting, Dead };
-enum class Element { Anemo, Geo, Electro, Cryo, Pyro, Hydro };
-
-struct UnitStats {
-    Owner owner = Owner::PlayerCtrl;
-    State state = State::Idle;
-    int hp = 0;
-    int max_hp = 0;
-    int atk = 0;
-    int range = 0;
-    int max_mana = 0;
-    int mana = 0;
-    int level = 1;
-    int shield = 0;
-    Equipment equipped = std::monostate{};
-};
-
-struct PyroSlime;
-struct HydroSlime;
-struct AnemoSlime;
-struct GeoSlime;
-struct ElectroSlime;
-struct CryoSlime;
-
-using Unit = std::variant<PyroSlime, HydroSlime, AnemoSlime, GeoSlime,
-                          ElectroSlime, CryoSlime>;
-
 namespace __tag {
 struct stats_t {};
 struct take_damage_t {};
